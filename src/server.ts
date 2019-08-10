185697748345def4191fa1c66fa488e463023bff
Edison Lim <edison.limjh@gmail.com>
2019-08-10 23:37:12 +0800

    refactor: start of a clean rewrite of kaya-rpc v0.3

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import http from "http";
+import express from "express";
+import { applyMiddleware } from "./utils";
+import middleware from "./middleware";
+
+const router = express();
+applyMiddleware(middleware, router);
+
+const { PORT = 4200 } = process.env;
+const server = http.createServer(router);
+
+server.listen(PORT, () =>
+  console.log(`Server is running http://localhost:${PORT}...`)
+);
